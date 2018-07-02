87a292d5f35537f9964555628ab2f81ba4a76432
Axel KUHN <kuax@protonmail.com>
2018-07-02 11:18:22 +0200

    Added BrowserSync and updated README

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # webdev-workflow
+
 Setup for SASS and ES6+ development workflow
 
 ## Getting started
 
-1. Add `gulpfile.js` and `webpack.config.js` to your project
-2. Configure src and dest paths in both those files
-3. Add `devDependencies` to **your** `package.json`
-4. `npm install`
-5. Start the pipeline with `gulp`
+1. Have `gulp-cli` installed globally
+2. Add `gulpfile.js` and `webpack.config.js` to your project
+3. Configure src and dest paths in both those files
+4. Configure browserSync's `port` and `proxy` to suit your needs
+5. Add `devDependencies` to **your** `package.json`
+6. `npm install`
+7. Start the pipeline with `gulp`
--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ var sourcemaps = require('gulp-sourcemaps');
 var webpack = require('webpack-stream');
 var log = require('fancy-log');
 var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
 
 var paths = {
     scss: './src/sass/**/*.scss',
-    js: './src/js/**/*.js'
+    js: './src/js/**/*.js',
+    cshtml: '../**/*.cshtml'
 };
 
 gulp.task('sass', function() {
@@ -24,12 +26,23 @@ gulp.task('js:webpack', function() {
     return gulp.src('./src/js/main.js')
                 .pipe(plumber())
                 .pipe(webpack(require('./webpack.config.js')).on('error', log.error))
-                .pipe(gulp.dest('./js'))
+                .pipe(gulp.dest('./js'));
 });
 
 gulp.task('watch', function() {
+
+    browserSync.init({
+        port: 3000,
+        proxy: "localhost:63123",
+        browser: []
+    });
+
     gulp.watch(paths.scss, ['sass']);
     gulp.watch(paths.js, ['js:webpack']);
+
+    gulp.watch('./js/*.js').on('change', browserSync.reload);
+    gulp.watch('./css/*.css').on('change', browserSync.reload);
+    gulp.watch(paths.cshtml).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['watch', 'sass']);
